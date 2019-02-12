0e8d53d225f63b6ae0006dc1e57318e716cf886f
Yang Song <ysonggit@users.noreply.github.com>
2019-02-12 20:11:36 +0000

    Update label_image.py

--- scripts/label_image.py
+++ scripts/label_image.py
@@ -70,7 +70,7 @@ def load_labels(label_file):
 if __name__ == "__main__":
   file_name = "tf_files/traffic_lights/yellow/left0249.jpg"
   model_file = "tf_files/retrained_graph.pb"
-  label_file = "tf_files/retrained_labels_tl.txt"
+  label_file = "tf_files/retrained_labels.txt"
   input_height = 224
   input_width = 224
   input_mean = 128
